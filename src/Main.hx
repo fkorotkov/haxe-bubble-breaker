55646dfa9d57d8158b6ee7106b8c672f6aa9ccb9
Fedor Korotkov <fedor.korotkov@jetbrains.com>
2012-06-19 17:30:35 +0400

    add ant task

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,6 @@
 package ;
 import org.haxe.bubble.Game;
 import nme.Lib;
-using StringTools;
 class Main {
     public static function main() {
         Lib.current.addChild(new Game());
--- src/org/haxe/bubble/ExtendedGame.hx
+++ src/org/haxe/bubble/ExtendedGame.hx
@@ -3,12 +3,10 @@ class ExtendedGame {
     var bar(getBar, setBar):Int;
     function foo():String {}
 
-    @:setter(bar)
     public function setBar(value:Int) {
         this.bar = value;
     }
 
-    @:getter(bar)
     public function getBar():Int {
         return bar;
     }
